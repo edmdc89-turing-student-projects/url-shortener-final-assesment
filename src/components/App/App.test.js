118a7e5805898008852ff7a3c9564c37b749f49a
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-08-04 12:03:38 -0600

    Test app integration with form and api

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { render, fireEvent, waitFor } from '@testing-library/react'
+import { render, fireEvent, waitFor, act } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App'
-import { getUrls } from '../../apiCalls'
+import { getUrls, shortenNewUrl } from '../../apiCalls'
 
 jest.mock('../../apiCalls')
 
@@ -26,7 +26,7 @@ describe('App', () => {
   })
 
   it('should get shortened url from api and render on DOM', async () => {
-    getUrls.mockResolvedValue({
+    getUrls.mockResolvedValueOnce({
       urls: [
         {
           id: 1,
@@ -49,4 +49,41 @@ describe('App', () => {
     expect(longUrlText).toBeInTheDocument()
   })
 
+  it('should get shortened url from api and render on DOM', async () => {
+    shortenNewUrl.mockResolvedValueOnce({
+      id: 1,
+      long_url: "http://some.lengthy.gibberish.com",
+      short_url: "http://localhost:3001/useshorturl/1",
+      title: "New Url"
+    })
+
+    const {
+      getByRole,
+      getByText,
+      getByPlaceholderText,
+      findByRole,
+      findByText
+    } = render(
+      <App />
+    )
+
+    const appMessage = getByText('No urls yet! Find some to shorten!')
+    expect(appMessage).toBeInTheDocument()
+
+    const titleInput = getByPlaceholderText('Title...')
+    const newUrlInput = getByPlaceholderText('URL to Shorten...')
+    const submitButton = getByRole('button', {name: 'Shorten Please!'})
+
+    fireEvent.change(titleInput, {target: {value: 'New Url'}})
+    fireEvent.change(newUrlInput, {target: {value: "http://some.lengthy.gibberish.com"}})
+    fireEvent.click(submitButton)
+
+    const urlTitle = await findByRole('heading', {name: 'New Url'})
+    const shortUrlLink = await findByRole('link', {name: "http://localhost:3001/useshorturl/1"})
+    const longUrlText = await findByText("http://some.lengthy.gibberish.com")
+
+    expect(urlTitle).toBeInTheDocument()
+    expect(shortUrlLink).toBeInTheDocument()
+    expect(longUrlText).toBeInTheDocument()
+  })
 })
