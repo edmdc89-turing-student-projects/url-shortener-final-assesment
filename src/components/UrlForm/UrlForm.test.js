5eacd4e2672afdbdb77fd4e79b3ab11a8b358299
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-08-04 11:08:45 -0600

    Test prop function call on submission

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -17,6 +17,7 @@ describe('Url form', () => {
     expect(newUrlInput).toBeInTheDocument()
     expect(submitButton).toBeInTheDocument()
   })
+
   it('should keep track of changes on the input', () => {
     const { getByRole, getByPlaceholderText } = render(
       <UrlForm />
@@ -30,5 +31,28 @@ describe('Url form', () => {
 
     expect(titleInput.value).toMatch('New Url')
     expect(newUrlInput.value).toMatch('http://some.lengthy.miscellaneous.url.com')
+  })
+
+  it('should call on a function to create short Url', () => {
+    const mockCreateShortUrl = jest.fn()
+    const { getByRole, getByPlaceholderText } = render(
+      <UrlForm createShortUrl={mockCreateShortUrl}/>
+    )
+
+    const titleInput = getByPlaceholderText('Title...')
+    const newUrlInput = getByPlaceholderText('URL to Shorten...')
+    const submitButton = getByRole('button', {name: 'Shorten Please!'})
+
+    fireEvent.change(titleInput, {target: {value: 'New Url'}})
+    fireEvent.change(newUrlInput, {target: {value: 'http://some.lengthy.miscellaneous.url.com'}})
+
+    expect(titleInput.value).toMatch('New Url')
+    expect(newUrlInput.value).toMatch('http://some.lengthy.miscellaneous.url.com')
+
+    fireEvent.click(submitButton)
+
+    expect(mockCreateShortUrl).toHaveBeenCalledTimes(1)
+    expect(titleInput.value).toMatch('')
+    expect(newUrlInput.value).toMatch('')
   })
 })
