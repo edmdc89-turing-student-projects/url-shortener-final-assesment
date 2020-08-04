10a5903aba41823b9c97e2221b13a660b8e8e56c
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-08-04 11:24:05 -0600

    Test app renders w/o crashing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,24 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './App'
 
+describe('App', () => {
+  it('should render without crashing', () => {
+    const { getByRole, getByPlaceholderText, getByText } = render(
+      <App />
+    )
+
+    const appTitle = getByRole('heading', {name: 'URL Shortener'})
+    const titleInput = getByPlaceholderText('Title...')
+    const newUrlInput = getByPlaceholderText('URL to Shorten...')
+    const submitButton = getByRole('button', {name: 'Shorten Please!'})
+    const appMessage = getByText('No urls yet! Find some to shorten!')
+
+    expect(appTitle).toBeInTheDocument()
+    expect(titleInput).toBeInTheDocument()
+    expect(newUrlInput).toBeInTheDocument()
+    expect(submitButton).toBeInTheDocument()
+    expect(appMessage).toBeInTheDocument()
+  })
+})
