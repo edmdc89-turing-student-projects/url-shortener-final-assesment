b632d016c26c5e2e562b7877931f17fbb080ff34
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-08-04 11:48:24 -0600

    Add async test to App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App'
+import { getUrls } from '../../apiCalls'
+
+jest.mock('../../apiCalls')
 
 describe('App', () => {
   it('should render without crashing', () => {
@@ -21,4 +24,29 @@ describe('App', () => {
     expect(submitButton).toBeInTheDocument()
     expect(appMessage).toBeInTheDocument()
   })
+
+  it('should get shortened url from api and render on DOM', async () => {
+    getUrls.mockResolvedValue({
+      urls: [
+        {
+          id: 1,
+          long_url: "http://some.lengthy.gibberish.com",
+          short_url: "http://localhost:3001/useshorturl/1",
+          title: "Awesome photo"
+        }
+      ]
+    })
+
+    const { getByRole, getByText } = render(
+      <App />
+    )
+    const urlTitle = await waitFor(() => getByRole('heading', {name: 'Awesome photo'}))
+    const shortUrlLink = await waitFor(() => getByRole('link', {name: "http://localhost:3001/useshorturl/1"}))
+    const longUrlText = await waitFor(() => getByText("http://some.lengthy.gibberish.com"))
+
+    expect(urlTitle).toBeInTheDocument()
+    expect(shortUrlLink).toBeInTheDocument()
+    expect(longUrlText).toBeInTheDocument()
+  })
+
 })
